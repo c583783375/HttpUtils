608f7bff7e45f330d41aa908ffa99a06bf74298b
583783375@qq.com <583783375@qq.com>
2017-11-20 12:06:33 +0800

    优化okhttp 上传 下载文件

--- app/src/main/java/com/sprint/www/httputils/App.java
+++ app/src/main/java/com/sprint/www/httputils/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.sprint.www.httputils.http.HttpProxy;
 import com.sprint.www.httputils.http.okhttp.OkHttpProcessor;
+import com.sprint.www.httputils.http.volley.VolleyProcessor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,8 +19,8 @@ public class App extends Application {
             headers.put("Charset", "UTF-8");
             headers.put("Content-Type", "application/x-javascript");
             headers.put("Accept-Encoding", "gzip,deflate");
-        HttpProxy.init(new OkHttpProcessor());
-//        HttpProxy.init(new VolleyProcessor(this));
+    //    HttpProxy.init(new OkHttpProcessor());
+        HttpProxy.init(new VolleyProcessor(this));
 //        HttpProxy.obtain()
 //                .setHeader(headers)
 //                .setCache(false);
